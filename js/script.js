c912cf5dea7210059ad8fb06a3ed94fc290f9003
Guy Roger Eboulet <eboulet87@gmail.com>
2020-01-24 15:35:35 +0100

    messaggio inserito

--- js/script.js
+++ js/script.js
@@ -13,10 +13,10 @@ function sendMessage() {
   var textMessage = $("input.inserimento").val();
   console.log(textMessage);
   if (textMessage.length != 0) {
-    var newMessage = $(".template.message").clone();
+    var newMessage = $(".template .messaggio-utente").clone();
     console.log(newMessage);
     newMessage.find(".messaggio-verde").text(textMessage);
-    $("#chat .messaggio-utente").append(newMessage);
+    $("#chat").append(newMessage);
 
   }
 
