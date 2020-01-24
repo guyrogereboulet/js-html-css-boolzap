8df2ff36fdd76c02e6b5807e3d26d4579e1538e8
Guy Roger Eboulet <eboulet87@gmail.com>
2020-01-24 15:09:16 +0100

    Inserimento messaggio

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
-$(document).ready(
-  function () {
-    $("#invia").click(
-      function (){
-        var text = $(".inserimento").val();
-        console.log(text);
-        var elementNew = $(".messaggio-verde").clone();
-        elementNew.append(text);
-        $(".messaggio-utente").append(elementNew);
+$(document).ready (function(){
+  $("#invia").click(function (){
 
-    });
+    sendMessage();
 
+  });
 
 
 });
+
+
+function sendMessage() {
+  var textMessage = $("input.inserimento").val();
+  console.log(textMessage);
+  if (textMessage.length != 0) {
+    var newMessage = $(".template.message").clone();
+    console.log(newMessage);
+    newMessage.find(".messaggio-verde").text(textMessage);
+    $("#chat .messaggio-utente").append(newMessage);
+
+  }
+
+}
