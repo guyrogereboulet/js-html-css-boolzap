4fa9bdd79a6b84b1ed5e1de167dd0bfbd9d6e85d
Guy Roger Eboulet <eboulet87@gmail.com>
2020-01-26 14:18:07 +0100

    Inserimento funzione ricerca

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ $(document).ready (function(){
 });
 
 
+
 function sendMessage() {
   var textMessage = $("input.inserimento").val();
   console.log(textMessage);
@@ -13,5 +14,23 @@ function sendMessage() {
     console.log(newMessage);
     newMessage.find(".messaggio-verde").text(textMessage);
     $("#chat").append(newMessage);
+  }
+}
+
+
+function filter () {
+  var filterValue, input, ul ,li, i;
+  input = document.getElementById('ricerca');
+  filterValue = input.value.toUpperCase();
+  ul = document.getElementById('contatti');
+  li = ul.getElementsByTagName('li');
+
+  for (var i = 0; i < li.length; i++) {
+    var a = li[i].getElementsByTagName('p') [0];
+    if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+      li[i].style.direction = "";
+    }else {
+      li[i].style.display = "none";
+    }
   }
 }
